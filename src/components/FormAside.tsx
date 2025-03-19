47bdbe7ade34f5d29c26b73c172ca28bb5de0e97
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-03-19 13:05:13 +0100

    add sidebard bg image

--- src/components/FormAside.tsx
+++ src/components/FormAside.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import asideBackground from "../assets/images/bg-sidebar-desktop.svg?url";
+import asideBackground from "../assets/images/bg-sidebar-desktop.svg";
 
 const Aside = styled.aside`
-  background-color: blue;
+  background: url("${asideBackground}") center/cover no-repeat;
   width: 100%;
   height: 100%;
 `;

f1d5593c64322a3dbdf223f4c2a582e7e9b931d4
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-03-26 16:13:45 +0100

    update date structure

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,6 +1,6 @@
-import { FormStep } from "../../types";
+import { FormEnding, FormStep } from "../../types";
 
-const getFormSteps = async (): Promise<FormStep[]> => {
+const getFormSteps = async (): Promise<{ steps: FormStep[], ending: FormEnding }> => {
   const data = await fetch(
     "https://raw.githubusercontent.com/damirdine/multi-step-form/refs/heads/main/src/lib/data/steps.json"
   );

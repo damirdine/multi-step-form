09d5e46e42175e248196898bd06d07d7e5fed166
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-04-16 10:42:14 +0200

    style radio input

--- src/components/ui/InputRadio.tsx
+++ src/components/ui/InputRadio.tsx
@@ -1,18 +1,52 @@
 import styled from "styled-components";
 import { InputOptions } from "../../types";
 
-const StyleRadioInput = styled.div`
+const StyleRadioInput = styled.section`
   display: flex;
-  gap: 4px;
-  padding: 15px;
-  margin-bottom: 5px;
-  & label {
-    display: grid;
-    /* padding: 35px; */
-    background-color: red;
+  gap: 18px;
+  
+  label {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    border: 1.6px solid var(--light-gray);
+    border-radius: 8px;
+    width: 108px;
+    min-width: 108px;
+    /*     height: 120px; */
+    &:hover {
+      border-color: var(--purplish-blue);
+      cursor: pointer;
+    }
+    img {
+      width: 46px;
+      margin-bottom: 42px;
+    }
+    div {
+      display: grid;
+      gap: 3.8px;
+      &:nth-child(1) {
+        font-weight: bold;
+        font-size: 1rem;
+        color: var(--marine-blue);
+      }
+      &:nth-child(2) {
+        color: var(--cool-gray);
+        font-size: 0.96rem;
+      }
+      &:nth-child(3) {
+        color: var(--marine-blue);
+        font-size: 0.88rem;
+      }
+    }
 
-    > label input:checked {
-      border: 3px solid blue;
+    input {
+      appearance: none;
+    }
+
+    &:has(input:checked) {
+      background-color: var(--magnolia);
+      border-color: var(--purplish-blue);
     }
   }
 `;
@@ -25,10 +59,12 @@ function InputRadio({ options, name }: InputRadioProps) {
       {options?.map((el) => (
         <label htmlFor={el.label} key={el.label}>
           <input type="radio" name={name} id={el.label} />
-          <p>
-            {el.label} - {el?.price?.monthly}
-          </p>
           <img src={el.image} />
+          <div>
+            <div>{el.label}</div>
+            <div>${el.price?.monthly}/mo</div>
+            <span>2 months free</span>
+          </div>
         </label>
       ))}
       <br />

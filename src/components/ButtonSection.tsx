786855f62a904b2e440bfe7c1b7b9ed5492af149
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-04-17 14:06:25 +0200

    fix button section

--- src/components/ButtonSection.tsx
+++ src/components/ButtonSection.tsx
@@ -1,44 +1,48 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import Button from "./ui/Button";
-import { useFormSteps } from '../context/useFormSteps';
+import { useFormSteps } from "../context/useFormSteps";
 
-const ButtonSectionStyled = styled.section`
+const ButtonSectionStyled = styled.section<{
+  justify?: string;
+}>`
   display: flex;
-  justify-content: space-between;
+  justify-content: ${({ justify }) => justify || "space-between"};
   margin-top: 38px;
 `;
 
 function ButtonSection() {
-    const context = useFormSteps();
-      if (!context?.currentStep) {
-        return;
-      }
-      const step = context?.currentStep;
-    return (
-        <ButtonSectionStyled>
-          {step.number === 1 || (
-            <Button
-              onClick={(e) => {
-                e.preventDefault();
-                context.goToStep(step.number - 1);
-              }}
-            >
-              Go back
-            </Button>
-          )}
-          {context.steps.length !== step.number && (
-            <Button
-              primary={true}
-              onClick={(e) => {
-                e.preventDefault();
-                context.goToStep(step.number + 1);
-              }}
-            >
-              Next Step
-            </Button>
-          )}
-        </ButtonSectionStyled>
-      );
+  const context = useFormSteps();
+  if (!context?.currentStep) {
+    return;
+  }
+  const step = context?.currentStep;
+  return (
+    <ButtonSectionStyled
+      justify={step.number === 1 ? "flex-end" : "space-between"}
+    >
+      {step.number === 1 || (
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            context.goToStep(step.number - 1);
+          }}
+        >
+          Go back
+        </Button>
+      )}
+      {context.steps.length !== step.number && (
+        <Button
+          primary={true}
+          onClick={(e) => {
+            e.preventDefault();
+            context.goToStep(step.number + 1);
+          }}
+        >
+          Next Step
+        </Button>
+      )}
+    </ButtonSectionStyled>
+  );
 }
 
-export default ButtonSection
+export default ButtonSection;

92882f018f473e3e889db5b1f8db00692e20f2bb
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-04-17 14:15:42 +0200

    implem ending

--- src/components/Ending.tsx
+++ src/components/Ending.tsx
@@ -1,18 +1,42 @@
+import styled from "styled-components";
 import { useFormSteps } from "../context/useFormSteps";
+import Button from "./ui/Button";
+
+const StyledEnding = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 38px;
+
+  div {
+    text-align: center;
+    margin: 0px 54px;
+
+    p {
+        margin-top: 18px;
+        color: var(--cool-gray)
+    }
+  }
+`;
 
 function Ending() {
   const context = useFormSteps();
   return (
-    <div>
+    <StyledEnding>
       <img
         src={
           "https://raw.githubusercontent.com/damirdine/multi-step-form/refs/heads/main/src/assets/images/icon-thank-you.svg"
         }
         alt="icon thank you"
       />
-      <h1>{context?.currentStep?.title}</h1>
-      <p>{context?.currentStep?.description}</p>
-    </div>
+      <div>
+        <h1>{context?.currentStep?.title}</h1>
+        <p>{context?.currentStep?.description}</p>
+      </div>
+
+      <Button>Restart</Button>
+    </StyledEnding>
   );
 }
 
